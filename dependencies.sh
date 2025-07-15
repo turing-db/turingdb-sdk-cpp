aec8c54512ce01c0a8c4e813f7d32da205fd4485
Remy Boutonnet <remy.boutonnet@turing.bio>
2025-07-15 18:07:25 +0200

    Fix dependencies script

--- dependencies.sh
+++ dependencies.sh
@@ -4,5 +4,5 @@ if [[ "$(uname -s)" == "Darwin" ]]; then
     brew install curl python3
 else
     sudo apt-get update -qqy
-    sudo apt-get install -qqy libcurl-dev python3-dev
+    sudo apt-get install -qqy libcurl4-openssl-dev curl python3-dev
 fi
